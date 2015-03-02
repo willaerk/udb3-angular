2b6f27ef8d1270399e599f2c47d9015816017666
Kristof Coomans <kristof@coomans.eu>
2015-03-02 19:37:05 +0100

    III-192: Add tabs

--- src/event-detail/ui/event-detail.controller.js
+++ src/event-detail/ui/event-detail.controller.js
@@ -12,6 +12,47 @@ angular
     .controller('EventDetailController', EventDetail);
 
 /* @ngInject */
-function EventDetail($scope, $routeParams) {
+function EventDetail($scope, $routeParams, $location) {
+  var getActiveTabId = function() {
+    var hash = $location.hash();
+
+    if (hash) {
+      return hash;
+    }
+    else {
+      return 'data';
+    }
+  };
+
+  var activeTabId = getActiveTabId();
+
   $scope.eventId = $routeParams.eventId;
+
+  $scope.tabs = [
+    {
+      id: 'data',
+      header: 'Gegevens',
+      active: false
+    },
+    {
+      id: 'history',
+      header: 'Historiek',
+      active: false
+    },
+    {
+      id: 'publication',
+      header: 'Publicatie',
+      active: false
+    }
+  ];
+
+  $scope.classForTab = function (tab) {
+    if (tab.id === activeTabId) {
+      return 'active';
+    }
+  };
+
+  $scope.$on('$locationChangeSuccess', function () {
+    activeTabId = getActiveTabId();
+  });
 }

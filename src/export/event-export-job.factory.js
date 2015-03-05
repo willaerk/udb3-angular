f0fedfd5d9f1e38c365fede7426c06cad08df12b
Bram Cordie <bramcordie@gmail.com>
2015-03-05 13:41:02 +0100

    III-186: Use file extension instead of format name in export description

--- src/export/event-export-job.factory.js
+++ src/export/event-export-job.factory.js
@@ -52,7 +52,8 @@ function EventExportJobFactory(BaseJob, JobStates) {
     if(this.state === JobStates.FAILED) {
       description = 'Exporteren van evenementen mislukt';
     } else {
-      description = 'Document .' + this.format + ' met ' + this.eventCount + ' evenementen';
+      var exportExtension = this.exportUrl.split('.').pop();
+      description = 'Document .' + exportExtension + ' met ' + this.eventCount + ' evenementen';
     }
 
     return description;

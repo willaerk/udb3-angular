d7657b3ed3001fa56193411f390d6d16495f0c3d
Kristof Coomans <kristof@coomans.eu>
2015-04-29 11:48:56 +0200

    III-7: Update dist files

--- dist/udb3-angular.js
+++ dist/udb3-angular.js
@@ -3900,6 +3900,38 @@ function udbExportModalButtons() {
   };
 }
 
+// Source: src/saved-searches/udb.saved-searches.service.js
+/**
+ * @ngdoc service
+ * @name udb.saved-searches.savedSearchesService
+ * @description
+ * # savedSearchesService
+ * Service in udb.saved-searches.
+ */
+angular
+  .module('udb.saved-searches')
+  .service('savedSearchesService', SavedSearchesService);
+
+/* @ngInject */
+function SavedSearchesService($q, $http, appConfig) {
+  var apiUrl = appConfig.baseApiUrl;
+  var defaultApiConfig = {
+    withCredentials: true,
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+
+  this.createSavedSearch = function(name, query) {
+    var post = {
+      name: name,
+      query: query
+    };
+    return $http.post(apiUrl + 'saved-searches', post, defaultApiConfig);
+  };
+}
+SavedSearchesService.$inject = ["$q", "$http", "appConfig"];
+
 // Source: src/saved-searches/ui/saved-searches-list.controller.js
 /**
  * @ngdoc function
